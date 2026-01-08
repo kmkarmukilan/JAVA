7dae21b76be8c15fc36a5665077d3856a3be18d5
kmkarmukilan <kmkarmukilan02@gmail.com>
2026-01-08 15:07:31 +0530

    Add files via upload

---
+++ Java.java
@@ -0,0 +1,8 @@
+public class Java {
+    public static void main(String[] args) {
+        boolean isJavaFun = true;
+        boolean isFishTasty = false;
+        System.out.println("Is Java fun? " + isJavaFun);
+        System.out.println("Is fish tasty? " + isFishTasty);
+    }
+}
