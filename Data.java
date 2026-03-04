97c2c7cb81387095f930e79159d5684b4a1f38f3
kmkarmukilan <kmkarmukilan02@gmail.com>
2026-03-04 22:30:25 +0530

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,18 @@
+public class Data{
+   public static void main(String[] args){
+	int a=10,b=20;
+	boolean c=true;
+	boolean d=false;
+	System.out.println("Sum "+ a+b);	
+	System.out.println("Difference "+ (a-b));		
+	System.out.println("Product "+ a*b);		
+	System.out.println("Quotient "+ a/b);		
+	System.out.println("Remainder "+ a%b);	
+	System.out.println("Increment "+ a++);	
+	System.out.println("Decrement "+ a--);
+	System.out.println("Value of a "+ a);				
+	System.out.println("AND "+ (c&&d));		
+	System.out.println("OR "+ (c||d));		
+	System.out.println("Not "+ (!c));	
+   }
+}				
