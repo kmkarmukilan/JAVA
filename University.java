8f60329ead52c656c3705684eaefbbecf6f12a39
kmkarmukilan <kmkarmukilan02@gmail.com>
2026-03-04 21:18:12 +0530

    Add files via upload

---
+++ University.java
@@ -0,0 +1,32 @@
+class Person {
+String name;
+void displayName() {
+System.out.println("Name: " + name);
+}
+}
+
+class Student extends Person {
+int rollNo;
+void displayRoll() {
+System.out.println("Roll No: " + rollNo);
+}
+}
+
+class CollegeStudent extends Student {
+String course;
+void displayCourse() {
+System.out.println("Course: " + course);
+}
+}
+
+public class University{
+public static void main(String[] args) {
+CollegeStudent cs = new CollegeStudent();
+cs.name = "Deepa";
+cs.rollNo = 12;
+cs.course = "CSE";
+cs.displayName();
+cs.displayRoll();
+cs.displayCourse();
+}
+}
